8fed6197d32bc8cdd086226e3966b1bce8d124ef
Irene <ivinentmercadal@iesjoanramis.org>
2022-05-26 18:03:42 +0200

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     
+    <form id="search">
+      Search <input name="query" v-model="searchQuery" />
+    </form>
+    <Grid
+      :heroes="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery"
+    >
+    </Grid>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Grid from '@/components/Grid.vue'
 
 export default {
   name: 'Home',
+  data: function (){
+    return {
+    searchQuery: "",
+    gridColumns: ["name", "power"],
+    gridData: [
+      { name: "Chuck Norris", power: Infinity },
+      { name: "Bruce Lee", power: 9000 },
+      { name: "Jackie Chan", power: 7000 },
+      { name: "Jet Li", power: 8000 }
+    ]
+    }
+  },
   components: {
-    HelloWorld
+    Grid
   }
 }
 </script>
